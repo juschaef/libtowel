3390a852fe108befcdb94e445b93cf4970820ff5
yyang <yan.yaang@gmail.com>
2015-01-24 19:21:10 +0100

    update dict 2

--- includes/twl_dict.h
+++ includes/twl_dict.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ldict.h                                            :+:      :+:    :+:   */
+/*   twl_dict.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juschaef <juschaef@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yyang <yyang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/09 13:20:27 by yyang             #+#    #+#             */
-/*   Updated: 2015/01/23 20:34:30 by juschaef         ###   ########.fr       */
+/*   Updated: 2015/01/24 19:21:00 by yyang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 void			twl_dict_set(t_lst *lst, char *key, void *data, void (*delfn)());
 void			twl_dict_add(t_lst *lst, char *key, void *data);
 void			*twl_dict_get(t_lst *lst, char *key);
-t_twl_lst_elem		*twl_dict_get_elem(t_lst *lst, char *key);
+t_twl_lst_elem	*twl_dict_get_elem(t_lst *lst, char *key);
 void			twl_dict_del_elem(t_lst *lst, char *key, void (*delfn)());
 
 #endif
