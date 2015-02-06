9ef4a7863d556940d97a4920ed2cf40032e72258
juschaef <juschaef@student.42.fr>
2015-02-06 18:56:24 +0100

    add xmalloc

--- srcs/twl_xstdlib/twl_xmalloc.c
+++ srcs/twl_xstdlib/twl_xmalloc.c
@@ -1,19 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   twl_xstdlib.h                                      :+:      :+:    :+:   */
+/*   twl_xmalloc.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juschaef <juschaef@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/01/24 19:24:29 by yyang             #+#    #+#             */
-/*   Updated: 2015/02/06 18:55:42 by juschaef         ###   ########.fr       */
+/*   Created: 2015/02/06 18:55:21 by juschaef          #+#    #+#             */
+/*   Updated: 2015/02/06 18:55:33 by juschaef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef TWL_XSTDLIB_H
-# define TWL_XSTDLIB_H
+#include <stdlib.h>
 
-char		*twl_joinpath(char *s1, char *s2);
-void		*twl_xmalloc(int size);
+void	*twl_xmalloc(int size)
+{
+	char *new;
 
-#endif
+	new = (char *)malloc(size);
+	if (!new)
+		exit(1);
+	return (new);
+}
