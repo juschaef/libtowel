8c12092d2decf3f700fa22b8ce27639579fc1e18
annguyen <annguyen@student.42.fr>
2015-02-12 12:09:06 +0100

    update dict

--- srcs/twl_dict/twl_dict_set.c
+++ srcs/twl_dict/twl_dict_set.c
@@ -3,17 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   twl_dict_set.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yyang <yyang@student.42.fr>                +#+  +:+       +#+        */
+/*   By: annguyen <annguyen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/09 13:21:53 by yyang             #+#    #+#             */
-/*   Updated: 2015/01/25 17:44:00 by yyang            ###   ########.fr       */
+/*   Updated: 2015/02/12 12:08:39 by annguyen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <twl_dict.h>
 #include <twl_string.h>
 
-void		twl_dict_set(t_twl_dict *dict, char *key, void *data, void (*delfn)())
+void		twl_dict_set(t_twl_dict *dict, char *key,
+												void *data, void (*delfn)())
 {
 	t_twl_dict_elem	*elem;
 
