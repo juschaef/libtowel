a40b6770db78fd15d56ce52c306577f12d051784
yyang42 <yan.yaang@gmail.com>
2015-02-24 18:39:54 +0100

    opt: handle -la

--- srcs/twl_opt/twl_opt_new.c
+++ srcs/twl_opt/twl_opt_new.c
@@ -6,21 +6,35 @@
 /*   By: yyang <yyang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/24 16:23:02 by yyang             #+#    #+#             */
-/*   Updated: 2015/02/24 18:33:58 by yyang            ###   ########.fr       */
+/*   Updated: 2015/02/24 18:39:34 by yyang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <twl_opt.h>
 #include <stdlib.h>
 #include <twl_arr.h>
+#include <twl_xstring.h>
 
-static void build_opt_dict(char **arr_opts, t_twl_dict *opts)
+static void parse_single_arg(char *arg, t_twl_dict *opts)
+{
+	char *opt_key;
+
+	while (*arg)
+	{
+		opt_key = twl_strnew(1);
+		*opt_key = *arg;
+		twl_dict_add(opts, opt_key, NULL);
+		arg++;
+	}
+}
+
+static void parse_argv(char **arr_opts, t_twl_dict *opts)
 {
 	while (*arr_opts)
 	{
 		if (**arr_opts != '-')
 			break ;
-		twl_dict_add(opts, twl_strdup(++(*arr_opts)), NULL);
+		parse_single_arg(++(*arr_opts), opts);
 		arr_opts++;
 	}
 }
@@ -33,6 +47,6 @@ t_twl_opt	*twl_opt_new(char **argv)
 	twl_opt->cmd = twl_strdup(argv[0]);
 	argv++;
 	twl_opt->opts = twl_dict_new();
-	build_opt_dict(argv, twl_opt->opts);
+	parse_argv(argv, twl_opt->opts);
 	return (twl_opt);
 }
--- tests/moulitest_tests/tests/twl_opt/twl_opt_exist.spec.c
+++ tests/moulitest_tests/tests/twl_opt/twl_opt_exist.spec.c
@@ -15,19 +15,19 @@ static void test_opt_cmd(t_test *test)
 	mt_assert(twl_opt_exist(twl_opt, "z") == FALSE);
 }
 
-// static void test_opt_cmd_harder(t_test *test)
-// {
-// 	char *argv[] = {"ls", "-l", NULL};
-// 	t_twl_opt *twl_opt;
+static void test_opt_cmd_harder(t_test *test)
+{
+	char *argv[] = {"ls", "-la", NULL};
+	t_twl_opt *twl_opt;
 
-// 	twl_opt = twl_opt_new(argv);
+	twl_opt = twl_opt_new(argv);
 	
-// 	mt_assert(twl_opt_exist(twl_opt, "l") == TRUE);
-// 	mt_assert(twl_opt_exist(twl_opt, "s") == FALSE);
-// }
+	mt_assert(twl_opt_exist(twl_opt, "l") == TRUE);
+	mt_assert(twl_opt_exist(twl_opt, "a") == TRUE);
+}
 
 void	suite_twl_opt_exist(t_suite *suite)
 {
 	SUITE_ADD_TEST(suite, test_opt_cmd);
-	// SUITE_ADD_TEST(suite, test_opt_cmd_harder);
+	SUITE_ADD_TEST(suite, test_opt_cmd_harder);
 }
