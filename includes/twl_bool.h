6c68c581517afdc6528115046adadb8bf9bf0aae
dump_bocal_1 <dump_bocal_1@pedago-2.42.fr>
2015-02-26 00:38:58 +0100

    union t_bool

--- includes/twl_bool.h
+++ includes/twl_bool.h
@@ -3,18 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   twl_bool.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juschaef <juschaef@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gbarraul <gbarraul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/21 14:54:03 by annguyen          #+#    #+#             */
-/*   Updated: 2015/01/30 15:06:14 by juschaef         ###   ########.fr       */
+/*   Updated: 2015/02/25 23:13:21 by gbarraul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TWL_BOOL_H
 # define TWL_BOOL_H
-# define TRUE 1
-# define FALSE 0
 
-typedef int	t_bool;
+typedef enum	e_bool
+{
+	FALSE,
+	TRUE
+}				t_bool;
 
 #endif
