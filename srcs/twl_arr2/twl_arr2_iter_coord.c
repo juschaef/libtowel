e2a644559b229419f52383da68aa8bfe82d3e8ff
juschaef <juschaef@student.42.fr>
2015-03-01 17:23:15 +0100

    norm

--- srcs/twl_arr2/twl_arr2_iter_coord.c
+++ srcs/twl_arr2/twl_arr2_iter_coord.c
@@ -6,17 +6,18 @@
 /*   By: juschaef <juschaef@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/28 14:35:01 by juschaef          #+#    #+#             */
-/*   Updated: 2015/02/28 22:03:31 by juschaef         ###   ########.fr       */
+/*   Updated: 2015/03/01 17:22:54 by juschaef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <twl_arr2.h>
 
-void	twl_arr2_iter_coord(void *arr2_, void (*iter_fn)(void *arr2, int y, int x, void *context), void *context)
+void	twl_arr2_iter_coord(void *arr2_,
+		void (*iter_fn)(void *arr2, int y, int x, void *context), void *context)
 {
-	int y;
-	int x;
-	void ***arr2;
+	int		y;
+	int		x;
+	void	***arr2;
 
 	arr2 = arr2_;
 	y = 0;
