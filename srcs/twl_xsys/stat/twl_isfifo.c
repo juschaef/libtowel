d07137634f001671a5cc49347339c58d9532cf1d
juschaef <juschaef@student.42.fr>
2015-03-07 14:37:40 +0100

    add some sys/stat functions

--- srcs/twl_xsys/stat/twl_isfifo.c
+++ srcs/twl_xsys/stat/twl_isfifo.c
@@ -1,25 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   stat.h                                             :+:      :+:    :+:   */
+/*   twl_isfifo.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juschaef <juschaef@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/01/20 21:12:52 by annguyen          #+#    #+#             */
-/*   Updated: 2015/03/07 14:36:58 by juschaef         ###   ########.fr       */
+/*   Created: 2015/01/20 21:12:40 by annguyen          #+#    #+#             */
+/*   Updated: 2015/03/07 14:30:14 by juschaef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef TWL_XSYS_STAT_H
-# define TWL_XSYS_STAT_H
+#include <sys/stat.h>
 
-int			twl_isdir(char *path);
-int			twl_isexec(char *path);
-int			twl_isblock(char *path);
-int			twl_issocket(char *path);
-int			twl_isfifo(char *path);
-int			twl_isspec_char(char *path);
-int			twl_islink(char *path);
-int			twl_isdir(char *path);
+int		twl_isfifo(char *path)
+{
+	struct stat st;
 
-#endif
+	lstat(path, &st);
+	return (S_ISFIFO(st.st_mode));
+}
--- srcs/twl_xsys/stat/twl_isspec_char.c
+++ srcs/twl_xsys/stat/twl_isspec_char.c
@@ -1,25 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   stat.h                                             :+:      :+:    :+:   */
+/*   twl_isspec_char.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juschaef <juschaef@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/01/20 21:12:52 by annguyen          #+#    #+#             */
-/*   Updated: 2015/03/07 14:36:58 by juschaef         ###   ########.fr       */
+/*   Created: 2015/01/20 21:12:40 by annguyen          #+#    #+#             */
+/*   Updated: 2015/03/07 14:29:57 by juschaef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef TWL_XSYS_STAT_H
-# define TWL_XSYS_STAT_H
+#include <sys/stat.h>
 
-int			twl_isdir(char *path);
-int			twl_isexec(char *path);
-int			twl_isblock(char *path);
-int			twl_issocket(char *path);
-int			twl_isfifo(char *path);
-int			twl_isspec_char(char *path);
-int			twl_islink(char *path);
-int			twl_isdir(char *path);
+int		twl_isspec_char(char *path)
+{
+	struct stat st;
 
-#endif
+	lstat(path, &st);
+	return (S_ISCHR(st.st_mode));
+}
