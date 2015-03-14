97723566da2c3abe6896a95188a92e7f8c792bc3
yyang42 <yan.yaang@gmail.com>
2015-03-14 11:08:18 +0100

    add color

---
+++ includes/twl_color.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_norris_loves_the_norminette.c                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chuck <chuck@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2042/02/30 42:00:00 by chuck             #+#    #+#             */
+/*   Updated: 2042/02/30 42:00:00 by chuck            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TWL_COLOR_H
+# define TWL_COLOR_H
+
+# define C_CLEAR "\033[0m"
+# define C_YELLOW "\033[33m"
+# define C_RED "\033[31;1m"
+# define C_GREEN "\033[32;1m"
+# define C_CYAN "\033[36;1m"
+# define C_WHITE "\033[37;1m"
+# define C_BLUE "\033[34;1m"
+
+#endif
