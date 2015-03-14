62318b6a8939fa58ab08b9e6b3e15610c7bfc47b
yyang42 <yan.yaang@gmail.com>
2015-03-14 11:08:06 +0100

    check norris is here

--- srcs/twl_lst/twl_lst_swap.c
+++ srcs/twl_lst/twl_lst_swap.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   check_norris_loves_the_norminette.c                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Chuck Noris <chuck@student.42.fr>          +#+  +:+       +#+        */
+/*   By: chuck <chuck@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2042/02/30 42:00:00 by chuck             #+#    #+#             */
 /*   Updated: 2042/02/30 42:00:00 by chuck            ###   ########.fr       */
