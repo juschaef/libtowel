5f8357d920c6fd0a377b3e22e9dd14ab24d0f0f7
yyang42 <yan.yaang@gmail.com>
2015-03-29 16:07:43 +0200

    add lst_findall

---
+++ srcs/twl_lst/twl_lst_findall.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_norris_loves_the_norminette.c                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chuck <chuck@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2042/02/30 42:00:00 by chuck             #+#    #+#             */
+/*   Updated: 2042/02/30 41:59:59 by chuck            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "twl_lst.h"
+
+t_lst				*twl_lst_findall(t_lst *lst, bool (*iter_fn)(void *data,
+												void *context), void *context)
+{
+	t_lst_elem__	*elem;
+	t_lst			*new_lst;
+
+	new_lst = twl_lst_new();
+	elem = lst->head;
+	while (elem)
+	{
+		if (iter_fn(elem->data, context))
+			twl_lst_push(new_lst, elem->data);
+		elem = elem->next;
+	}
+	return (new_lst);
+}
---
+++ tests/moulitest_tests/tests/twl_lst/twl_lst_findall.spec.c
@@ -0,0 +1,39 @@
+#include <project.h>
+#include "twl_lst.h"
+
+bool find_fn(void *data, void *context)
+{
+	char *str1;
+	char *str2;
+
+	str1 = data;
+	str2 = context;
+	if (*str1 == *str2)
+		return (1);
+	return (0);
+}
+
+static void simple_test(t_test *test)
+{
+	t_lst	*lst;
+	t_lst	*matches;
+
+	lst = twl_lst_new();
+	twl_lst_push(lst, "aaa");
+	twl_lst_push(lst, "bbb");
+	twl_lst_push(lst, "abc");
+	twl_lst_push(lst, "azz");
+	twl_lst_push(lst, "zzz");
+
+	matches = twl_lst_findall(lst, find_fn, "a");
+	mt_assert(strcmp(twl_lst_get(matches, 0), "aaa") == 0);
+	mt_assert(strcmp(twl_lst_get(matches, 1), "abc") == 0);
+	mt_assert(strcmp(twl_lst_get(matches, 2), "azz") == 0);
+	mt_assert(twl_lst_len(matches) == 3);
+	free(lst);
+}
+
+void	suite_twl_lst_findall(t_suite *suite)
+{
+	SUITE_ADD_TEST(suite, simple_test);
+}
