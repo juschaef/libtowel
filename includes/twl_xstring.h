b018adc94c6d1e80f48c5ddd23e7109dfee534db
yyang42 <yan.yaang@gmail.com>
2015-04-03 11:30:06 +0200

    add str_to_av

--- includes/twl_xstring.h
+++ includes/twl_xstring.h
@@ -69,5 +69,6 @@ size_t				twl_wstrlen(wchar_t *wstr);
 char				*twl_strjoinarr(const char **arr, const char *sep);
 char				*twl_strjoinlst(t_lst *lst, const char *sep);
 void				twl_add_char(char *s, char c);
+char				**twl_str_to_av(char *str);
 
 #endif
--- tests/moulitest_tests/tests/twl_xstring/twl_strsplit_mul.spec.c
+++ tests/moulitest_tests/tests/twl_xstring/twl_strsplit_mul.spec.c
@@ -32,7 +32,6 @@ static void simple_split_border_end(t_test *test)
 	mt_assert(strcmp(segs[1], "tout") == 0);
 	mt_assert(segs[2] == 0);
 	mt_assert(twl_arr_len(segs) == 2);
-	twl_arr_str_print(segs, "+");
 }
 
 void	suite_twl_strsplit_mul(t_suite *suite)
