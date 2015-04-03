9fdf5888fe00b83c93aa3866ac267a46e552b2ca
juschaef <juschaef@student.42.fr>
2015-04-03 11:08:11 +0200

    add opt

--- srcs/twl_opt/twl_opt_args_len.c
+++ srcs/twl_opt/twl_opt_args_len.c
@@ -10,27 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef TWL_OPT_H
-# define TWL_OPT_H
+#include "twl_opt.h"
+#include "twl_lst.h"
 
-# include "twl_lst.h"
-
-typedef struct		s_opt
+size_t				twl_opt_args_len(t_opt *opt)
 {
-	char			*cmd;
-	t_lst			*opts;
-	t_lst			*args;
-	char			*valid_opts;
-}					t_opt;
-
-t_opt				*twl_opt_new(char **argv, char *valid_opts);
-void				twl_opt_del(t_opt *opt);
-bool				twl_opt_exist(t_opt *opt, char *opt_key);
-char				*twl_opt_check_invalid_opts(t_opt *opt);
-
-char				**twl_opt_new_parse_arg_opt_and_return_non_opt_args__(
-								char **arr_opts, t_opt *opt, char *valid_opts);
-size_t				twl_opt_args_len(t_opt *opt);
-char				*twl_opt_args_get(t_opt *opt, int index);
-
-#endif
+	return (twl_lst_len(opt->args));
+}
