171962504b2f2a80de24d64cb549dfdb9ec46dd0
yyang42 <yan.yaang@gmail.com>
2015-07-01 13:51:29 +0200

    xstdio: add file to str

--- srcs/twl_xstdio/twl_file_to_str.c
+++ srcs/twl_xstdio/twl_file_to_str.c
@@ -10,18 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef TWL_XSTDIO_H
-# define TWL_XSTDIO_H
+#include <unistd.h>
+#include <fcntl.h>
 
-void				twl_putstr(char const *s);
-void				twl_putstr_fd(char const *s, int fd);
-void				twl_putnchar(int n, char c);
-void				twl_putchar_fd(char c, int fd);
-int					twl_putchar_tty(int c);
-void				twl_putnstr_fd(char const *s, int n, int fd);
-int					twl_lprintf(const char *fmt, ...);
-void				twl_xprintf(const char *fmt, ...);
-void				twl_nxprintf(const char *fmt, ...);
-char				*twl_file_to_str(char *file_name);
+#include "twl_string.h"
+#include "twl_xstdio.h"
 
-#endif
+#define MAX_FILESIZE 2 * 1000 * 1000
+#define BUFF_READ_SIZE 42
+
+char				*twl_file_to_str(char *file_name)
+{
+	char			buf[MAX_FILESIZE];
+	char			*ptr;
+	int				ret;
+	int				fd;
+
+	twl_bzero(buf, MAX_FILESIZE);
+	ptr = buf;
+	fd = open(file_name, O_RDONLY);
+	if (fd == -1)
+		twl_xprintf("[ERROR] Can't open: %s\n", file_name);
+	while ((ret = read(fd, ptr, BUFF_READ_SIZE)) > 0)
+	{
+		ptr += ret;
+		if ((ptr - buf) > (MAX_FILESIZE - BUFF_READ_SIZE - 1))
+			twl_xprintf("[ERROR] File too large: %s\n", file_name);
+	}
+	if (ret == -1)
+		twl_xprintf("[ERROR] Read error: %s\n", file_name);
+	return (twl_strdup(buf));
+}
---
+++ tests/moulitest_tests/tests/twl_xstdio/twl_file_to_str.spec.c
@@ -0,0 +1,19 @@
+#include <project.h>
+#include "twl_xstring.h"
+#include "twl_xstdio.h"
+
+static void test_simple(t_test *test)
+{
+	char			cmd[500];
+	char			*control_str = "abc1234242";
+	char			*filename = "/tmp/twl_file_to_str.test_file";
+
+	sprintf(cmd, "echo '%s' > %s", control_str, filename);
+	system(cmd);
+	mt_assert(strcmp(twl_strtrim(twl_file_to_str(filename)), control_str) == 0);
+}
+
+void	suite_twl_file_to_str(t_suite *suite)
+{
+	SUITE_ADD_TEST(suite, test_simple);
+}
