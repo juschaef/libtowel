896a12cce10b50607940bcd600facced125b3e29
juschaef <juschaef@student.42.fr>
2015-07-13 11:36:26 +0200

    graph: add neighbors data

---
+++ srcs/twl_graph/twl_graph_get_neighbors_data.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_norris_loves_the_norminette.c                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chuck <chuck@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2042/02/30 42:00:00 by chuck             #+#    #+#             */
+/*   Updated: 2042/02/30 41:59:59 by chuck            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "twl_graph.h"
+#include "twl_xstdlib.h"
+#include "twl_xstdio.h"
+
+t_lst				*twl_graph_get_neighbors_data(t_graph *graph,
+													t_graph_node_id node_id)
+{
+	return (twl_graph_node_neighbors_data(twl_graph_node_mgr_find_by_id(graph->nodes_, node_id)));
+
+}
