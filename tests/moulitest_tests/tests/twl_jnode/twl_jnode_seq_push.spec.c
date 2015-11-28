d9287df496dc3b5402b15aadd213bd64c711a82d
yyang42 <yaoyanyang42@gmail.com>
2015-11-29 00:05:12 +0100

    json: add jnode_seq_push tests

--- tests/moulitest_tests/tests/twl_jnode/twl_jnode_seq_push.spec.c
+++ tests/moulitest_tests/tests/twl_jnode/twl_jnode_seq_push.spec.c
@@ -34,8 +34,31 @@ static void simple_test2(t_test *test)
 	twl_jnode_del(node1);
 }
 
+static void simple_test3(t_test *test)
+{
+	t_jnode		*node1;
+	t_jnode		*seq_node;
+	t_jnode		*seq_node2;
+
+	node1 = twl_jnode_new_prim(JSON_NUMBER, 1);
+	seq_node = twl_jnode_new_seq(JSON_ARRAY);
+	seq_node2 = twl_jnode_new_seq(JSON_ARRAY);
+	twl_jnode_seq_push(seq_node, seq_node2);
+	twl_jnode_seq_push(seq_node2, node1);
+	twl_jnode_seq_push(seq_node2, node1);
+	twl_jnode_seq_push(seq_node2, node1);
+	mt_assert(twl_lst_len(seq_node->value.seq) == 1);
+	t_jnode 		*node;
+	node = twl_lst_first(seq_node->value.seq);
+	mt_assert(twl_lst_len(node->value.seq) == 3);
+	twl_jnode_del(seq_node);
+	twl_jnode_del(seq_node2);
+	twl_jnode_del(node1);
+}
+
 void	suite_twl_jnode_seq_push(t_suite *suite)
 {
 	SUITE_ADD_TEST(suite, simple_test);
 	SUITE_ADD_TEST(suite, simple_test2);
+	SUITE_ADD_TEST(suite, simple_test3);
 }
