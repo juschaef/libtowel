96ba6d4765242d76d22f698bf3571152094c8014
Ludovic CHENUT <lchenut@e2r11p17.42.fr>
2015-12-08 19:28:15 +0100

    forgot isgraph...

--- srcs/twl_ctype/twl_isgraph.c
+++ srcs/twl_ctype/twl_isgraph.c
@@ -6,27 +6,13 @@
 /*   By: chuck <chuck@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2042/02/30 42:00:00 by chuck             #+#    #+#             */
-/*   Updated: 2042/02/30 41:59:59 by chuck            ###   ########.fr       */
+/*   Updated: 2042/02/30 42:59:59 by chuck            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef TWL_CTYPE_H
-# define TWL_CTYPE_H
-
-int					twl_isalnum(int c);
-int					twl_isalpha(int c);
-int					twl_isascii(int c);
-int					twl_isblank(int c);
-int					twl_isdigit(int c);
-int					twl_islower(int c);
-int					twl_isprint(int c);
-int					twl_ispunct(int c);
-int					twl_isspace(int c);
-int					twl_isupper(int c);
-int					twl_tolower(int c);
-int					twl_toupper(int c);
-int					twl_iscntrl(int c);
-int					twl_isxdigit(int c);
-int					twl_isgraph(int c);
-
-#endif
+int				twl_isgraph(int c)
+{
+	if (c >= 041 && c <= 0176)
+		return (1);
+	return (0);
+}
