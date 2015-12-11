48a2133460132179313fa8426d440156855f4e21
yyang42 <yaoyanyang42@gmail.com>
2015-12-11 20:44:48 +0100

    lst: add tests for  ast_to_str

--- tests/moulitest_tests/tests/twl_lst/twl_lst_strjoin.spec.c
+++ tests/moulitest_tests/tests/twl_lst/twl_lst_strjoin.spec.c
@@ -17,7 +17,37 @@ static void simple_test(t_test *test)
 	free(s);
 }
 
+static void test_empty_strings(t_test *test)
+{
+	t_lst	*lst;
+	char	*s;
+
+	lst = twl_lst_new();
+
+	twl_lst_push(lst, strdup(""));
+	twl_lst_push(lst, strdup(""));
+	twl_lst_push(lst, strdup(""));
+
+	s = twl_lst_strjoin(lst, "|");
+	mt_assert(strcmp(s, "||") == 0);
+	free(s);
+}
+
+static void test_empty_list(t_test *test)
+{
+	t_lst	*lst;
+	char	*s;
+
+	lst = twl_lst_new();
+
+	s = twl_lst_strjoin(lst, "|");
+	mt_assert(strcmp(s, "") == 0);
+	free(s);
+}
+
 void	suite_twl_lst_strjoin(t_suite *suite)
 {
 	SUITE_ADD_TEST(suite, simple_test);
+	SUITE_ADD_TEST(suite, test_empty_strings);
+	SUITE_ADD_TEST(suite, test_empty_list);
 }
