2d1ff9232ea63294aa07ac34697e629952a00ed0
yyang42 <yaoyanyang42@gmail.com>
2015-12-17 19:05:18 +0100

    printf: fix valgrind invalid read

--- tests/moulitest_tests/tests/twl_stdio/twl_asprintf.spec.c
+++ tests/moulitest_tests/tests/twl_stdio/twl_asprintf.spec.c
@@ -17,8 +17,18 @@ static void test_digit(t_test *test)
 	mt_assert(strcmp(s, "{         001234}") == 0);
 }
 
+static void test_mixed(t_test *test)
+{
+	char *s;
+
+	twl_asprintf(&s, "%s%p%d%d%p%s%p%p%s",
+		"a", &free, 1, 2, &malloc, "b", &free, &malloc, "c");
+	mt_assert(strcmp(s, "a0x7fff9f823ec7120x7fff9f8210b2b0x7fff9f823ec70x7fff9f8210b2c") == 0);
+}
+
 void	suite_twl_asprintf(t_suite *suite)
 {
 	SUITE_ADD_TEST(suite, test_string);
 	SUITE_ADD_TEST(suite, test_digit);
+	SUITE_ADD_TEST(suite, test_mixed);
 }
