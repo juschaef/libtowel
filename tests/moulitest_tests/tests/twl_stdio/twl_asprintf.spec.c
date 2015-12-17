46631854e43787d3d8f9ae5ddb4f3f0e8544f755
yyang42 <yaoyanyang42@gmail.com>
2015-12-17 19:21:56 +0100

    printf: fix printf tests

--- tests/moulitest_tests/tests/twl_stdio/twl_asprintf.spec.c
+++ tests/moulitest_tests/tests/twl_stdio/twl_asprintf.spec.c
@@ -19,12 +19,14 @@ static void test_digit(t_test *test)
 
 static void test_mixed(t_test *test)
 {
-	char *s;
+	char *s1;
+	char *s2;
 
-	twl_asprintf(&s, "%s%p%d%d%p%s%p%p%s",
-		"a", &free, 1, 2, &malloc, "b", &free, &malloc, "c");
-	printf("s [%s]\n", s);
-	mt_assert(strcmp(s, "a0x7fff9f823ec7120x7fff9f8210b2b0x7fff9f823ec70x7fff9f8210b2c") == 0);
+	twl_asprintf(&s1, "%s%p%d%d%p%s%p%p%s", "a", &free, 1, 2, &malloc, "b", &free, &malloc, "c");
+	asprintf(&s2, "%s%p%d%d%p%s%p%p%s", "a", &free, 1, 2, &malloc, "b", &free, &malloc, "c");
+	mt_assert(strcmp(s1, s2) == 0);
+	free(s1);
+	free(s2);
 }
 
 void	suite_twl_asprintf(t_suite *suite)
