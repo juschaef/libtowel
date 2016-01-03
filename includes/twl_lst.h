a081832766e2e3db691a96d7761622bb8f2c3a36
yyang42 <yaoyanyang42@gmail.com>
2016-01-03 16:29:07 +0100

    lst: add iter3

--- includes/twl_lst.h
+++ includes/twl_lst.h
@@ -111,6 +111,9 @@ void						twl_lst_iter2n(t_lst *lst,
 		void (*f)(void *data, void *next, void *ctx1, void *ctx2),
 		void *ctx1, void *ctx2);
 void						twl_lst_iter0(t_lst *lst, void (*f)(void *data));
+void				twl_lst_iter3(t_lst *lst,
+		void (*f)(void *data, void *ctx1, void *ctx2, void *ctx3),
+		void *ctx1, void *ctx2, void *ctx3);
 
 void						twl_lst_remove_if(t_lst *lst,
 	bool (*filter_fn)(void *data, void *context),
