222e01b47d99fa5dcd1fb3f303d2834fbfebc832
yyang42 <yaoyanyang42@gmail.com>
2016-03-03 21:08:42 +0100

    xstring: add twl_str_split_once_to_lst tests

--- tests/moulitest_tests/tests/twl_xstring/twl_str_split_once_to_lst.spec.c
+++ tests/moulitest_tests/tests/twl_xstring/twl_str_split_once_to_lst.spec.c
@@ -8,6 +8,7 @@ static void simple_split(t_test *test)
 	segs = twl_str_split_once_to_lst("aaa bbb ccc", " ");
 	mt_assert(strcmp(twl_lst_get(segs, 0), "aaa") == 0);
 	mt_assert(strcmp(twl_lst_get(segs, 1), "bbb ccc") == 0);
+	mt_assert(twl_lst_len(segs) == 2);
 	twl_lst_del(segs, free);
 }
 
@@ -18,6 +19,7 @@ static void simple2_split(t_test *test)
 	segs = twl_str_split_once_to_lst("aaa   bbb   ccc  ", " ");
 	mt_assert(strcmp(twl_lst_get(segs, 0), "aaa") == 0);
 	mt_assert(strcmp(twl_lst_get(segs, 1), "  bbb   ccc  ") == 0);
+	mt_assert(twl_lst_len(segs) == 2);
 	twl_lst_del(segs, free);
 }
 
@@ -28,6 +30,18 @@ static void simple_equal_split(t_test *test)
 	segs = twl_str_split_once_to_lst("aaa=bbb=ccc", "=");
 	mt_assert(strcmp(twl_lst_get(segs, 0), "aaa") == 0);
 	mt_assert(strcmp(twl_lst_get(segs, 1), "bbb=ccc") == 0);
+	mt_assert(twl_lst_len(segs) == 2);
+	twl_lst_del(segs, free);
+}
+
+static void simple_multi_split(t_test *test)
+{
+	t_lst			*segs;
+
+	segs = twl_str_split_once_to_lst("aaa/bbb=ccc", "=/");
+	mt_assert(strcmp(twl_lst_get(segs, 0), "aaa") == 0);
+	mt_assert(strcmp(twl_lst_get(segs, 1), "bbb=ccc") == 0);
+	mt_assert(twl_lst_len(segs) == 2);
 	twl_lst_del(segs, free);
 }
 
@@ -44,5 +58,6 @@ void	suite_twl_str_split_once_to_lst(t_suite *suite)
 	SUITE_ADD_TEST(suite, simple_split);
 	SUITE_ADD_TEST(suite, simple2_split);
 	SUITE_ADD_TEST(suite, simple_equal_split);
+	SUITE_ADD_TEST(suite, simple_multi_split);
 	SUITE_ADD_TEST(suite, simple_nothing);
 }
