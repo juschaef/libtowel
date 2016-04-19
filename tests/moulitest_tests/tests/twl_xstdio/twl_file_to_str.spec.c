0d888c4fae3769388a5ffae2fa890c2d7290ffd8
yyang42 <yaoyanyang42@gmail.com>
2016-04-19 21:28:08 +0200

    feat/gnl_new: update twl_file_to_str 2

--- tests/moulitest_tests/tests/twl_xstdio/twl_file_to_str.spec.c
+++ tests/moulitest_tests/tests/twl_xstdio/twl_file_to_str.spec.c
@@ -13,7 +13,13 @@ static void test_simple(t_test *test)
 	mt_assert(strcmp(twl_strtrim(twl_file_to_str(filename)), control_str) == 0);
 }
 
+static void test_error(t_test *test)
+{
+	mt_assert(twl_file_to_str("/tmp/blablabla_not_exist") == 0);
+}
+
 void	suite_twl_file_to_str(t_suite *suite)
 {
 	SUITE_ADD_TEST(suite, test_simple);
+	SUITE_ADD_TEST(suite, test_error);
 }
