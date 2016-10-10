b8fa4f29a4b1ba9d7f6744efbb15f22a59e287c2
Ludovic CHENUT <chenut.ludovic@gmail.com>
2016-10-10 14:05:16 +0200

    remove old and neverused struct

--- includes/twl_stdio.h
+++ includes/twl_stdio.h
@@ -13,13 +13,6 @@
 #ifndef TWL_STDIO_H
 # define TWL_STDIO_H
 
-typedef struct		s_error_info
-{
-	int				value;
-	char			*name;
-	char			*msg;
-}					t_error_info;
-
 int					twl_printf(const char *fmt, ...);
 int					twl_sprintf(char *out, const char *fmt, ...);
 int					twl_asprintf(char **s, const char *fmt, ...);
